9de9de8fda4cf938ca9469c796b35122767be3e5
Matskir <89579318+Matskir@users.noreply.github.com>
2021-09-02 11:59:27 +0200

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+//Single for-loop
+double s = 0;
+for (int i = 0; i <= 100; i++){
+  s += i;
+}
+
+// Single while-loop
+int i = 0;
+double h = 0;
+while (i <= 100){
+  h += i;
+  i++
+}
+
+int main(){
+  std::cout << s;
+  std::cout << h;
+  return 0;
+}
