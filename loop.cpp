ad6f0d62ba7884e16cf08344f08b4c1221c6ee06
Matskir <89579318+Matskir@users.noreply.github.com>
2021-09-02 12:14:46 +0200

    Update loop.cpp

--- loop.cpp
+++ loop.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 
-//Single for-loop
-double s = 0;
-for (int i = 0; i <= 100; i++){
-  s += i;
-}
-
-// Single while-loop
-int i = 0;
-double h = 0;
-while (i <= 100){
-  h += i;
-  i++
-}
-
 int main(){
+  
+  //Single for-loop
+  double s = 0;
+  for (int i = 0; i <= 100; i++){
+    s += i;
+  }
+  
+  // Single while-loop
+  int i = 0;
+  double h = 0;
+  while (i <= 100){
+    h += i;
+    i++
+  }
+
   std::cout << s;
   std::cout << h;
   return 0;
